d4d83c97a8bab287e35fab98fbc6edbe1bc809c3
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2025-03-21 13:11:34 +0530

    changes

--- generate-package.bat
+++ generate-package.bat
@@ -4,30 +4,52 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 echo ^<?xml version="1.0" encoding="UTF-8"?^> > package.xml
 echo ^<Package xmlns="http://soap.sforce.com/2006/04/metadata"^> >> package.xml
 
-REM Get changed files using git show --name-only
-for /f "tokens=*" %%i in ('git show --name-only --pretty^=') do (
+REM Initialize empty variables
+set "apexClasses="
+set "apexTriggers="
+set "lightningComponents="
+
+REM Get only changed files
+for /f "tokens=*" %%i in ('git diff --name-only HEAD^ HEAD') do (
     set "filePath=%%i"
-    set "fileName=%%~ni"
-    set "metaType=%%~dpi"
-
-    if /i "!metaType!"=="\force-app\main\default\classes\" (
-        echo   ^<members^>!fileName!^</members^> >> package.xml
-        echo   ^<name^>ApexClass^</name^> >> package.xml
-    )
     
-    if /i "!metaType!"=="\force-app\main\default\triggers\" (
-        echo   ^<members^>!fileName!^</members^> >> package.xml
-        echo   ^<name^>ApexTrigger^</name^> >> package.xml
+    REM Extract filename without extension
+    for %%a in (!filePath!) do set "fileName=%%~na"
+
+    REM Check for Apex Classes
+    echo !filePath! | findstr /I /C:"force-app\main\default\classes\" >nul && (
+        set "apexClasses=!apexClasses!   ^<members^>!fileName!^</members^> & echo Class: !fileName!"
     )
 
-    if /i "!metaType!"=="\force-app\main\default\lwc\" (
-        echo   ^<members^>!fileName!^</members^> >> package.xml
-        echo   ^<name^>LightningComponentBundle^</name^> >> package.xml
+    REM Check for Apex Triggers
+    echo !filePath! | findstr /I /C:"force-app\main\default\triggers\" >nul && (
+        set "apexTriggers=!apexTriggers!   ^<members^>!fileName!^</members^> & echo Trigger: !fileName!"
     )
+
+    REM Check for Lightning Web Components
+    echo !filePath! | findstr /I /C:"force-app\main\default\lwc\" >nul && (
+        set "lightningComponents=!lightningComponents!   ^<members^>!fileName!^</members^> & echo LWC: !fileName!"
+    )
+)
+
+REM Write only changed files into package.xml
+if defined apexClasses (
+    echo !apexClasses! >> package.xml
+    echo   ^<name^>ApexClass^</name^> >> package.xml
+)
+
+if defined apexTriggers (
+    echo !apexTriggers! >> package.xml
+    echo   ^<name^>ApexTrigger^</name^> >> package.xml
+)
+
+if defined lightningComponents (
+    echo !lightningComponents! >> package.xml
+    echo   ^<name^>LightningComponentBundle^</name^> >> package.xml
 )
 
 echo ^<version^>63.0^</version^> >> package.xml
 echo ^</Package^> >> package.xml
 
-echo Package.xml generated successfully!
+echo Package.xml generated successfully with changed files only!
 ENDLOCAL
